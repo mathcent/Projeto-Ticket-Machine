c4bcda0e042be6d7015cf8f22764f119d0515d1f
unknown <bruno.mq@hotmail.com>
2022-09-24 00:02:10 -0300

    correcao classe Troco

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -11,34 +11,46 @@ class Troco {
     protected PapelMoeda[] papeisMoeda;
 
     public Troco(int valor) {
-        papeisMoeda = new PapelMoeda[6];
+        papeisMoeda = new PapelMoeda[7];
         int count = 0;
+        while (valor % 200 != 0) {
+            valor = valor - 200;
+            count++;
+        }
+        papeisMoeda[6] = new PapelMoeda(200, count);
+        count = 0;
         while (valor % 100 != 0) {
+            valor = valor - 100;
             count++;
         }
         papeisMoeda[5] = new PapelMoeda(100, count);
         count = 0;
         while (valor % 50 != 0) {
+            valor = valor - 50;
             count++;
         }
-        papeisMoeda[4] = new PapelMoeda(50, count);
+        papeisMoeda[5] = new PapelMoeda(50, count);
         count = 0;
         while (valor % 20 != 0) {
+            valor = valor - 20;
             count++;
         }
-        papeisMoeda[3] = new PapelMoeda(20, count);
+        papeisMoeda[4] = new PapelMoeda(20, count);
         count = 0;
         while (valor % 10 != 0) {
+            valor = valor - 10;
             count++;
         }
         papeisMoeda[2] = new PapelMoeda(10, count);
         count = 0;
         while (valor % 5 != 0) {
+            valor = valor - 5;
             count++;
         }
         papeisMoeda[1] = new PapelMoeda(5, count);
         count = 0;
         while (valor % 2 != 0) {
+            valor = valor - 2;
             count++;
         }
         papeisMoeda[0] = new PapelMoeda(2, count);
@@ -59,7 +71,7 @@ class Troco {
 
         @Override
         public boolean hasNext() {
-            for (int i = 6; i > 0; i--) {
+            for (int i = 6; i >= 0; i--) {
                 if (troco.papeisMoeda[i] != null) {
                     return true;
                 }
@@ -70,7 +82,7 @@ class Troco {
         @Override
         public PapelMoeda next() {
             PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret == null; i++) {
+            for (int i = 6; i >= 0 && ret == null; i--) {
                 if (troco.papeisMoeda[i] != null) {
                     ret = troco.papeisMoeda[i];
                     troco.papeisMoeda[i] = null;
